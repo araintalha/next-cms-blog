9cacafe34201476132abc84ff9abce557bfa444e
Talha Arain <ta031637@gmail.com>
2024-05-11 06:21:20 -0700

    first commit

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { post } from './schemas/post'
-import { tag } from './schemas/Tag'
+import { tag } from './schemas/tag'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [post,tag],
